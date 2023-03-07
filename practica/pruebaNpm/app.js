cdee82e09d7172589dfc69aaf3e5d7d6d0a2e486
Catalina de Arzubiaga Menghi <89262239+CatalinaMaria@users.noreply.github.com>
2023-03-07 10:29:47 -0300

    Update app.js

--- practica/pruebaNpm/app.js
+++ practica/pruebaNpm/app.js
@@ -4,13 +4,11 @@ console.log(movieQuotes.getRandomQuote());
 const listaAutos = require('./db/index');
 console.log(listaAutos);
 
-//function describirse() {
-//   return 'hola soy el auto ' + listaAutos.marca + ' , modelo ' + listaAutos.modelo + ' . Fabricado en el año ' + listaAutos.anio + ' y del color ' + listaAutos.color
-//}
-//console.log(describirse(1));
+function describirse() {
+   return 'hola soy el auto ' + listaAutos.marca + ' , modelo ' + listaAutos.modelo + ' . Fabricado en el año ' + listaAutos.anio + ' y del color ' + listaAutos.color
+}
+console.log(describirse(1));
 
 for (let i = 0; i < listaAutos.length; i++) {
-    function describirse() {
-        return 'hola soy el auto ' + listaAutos.marca + ' , modelo ' + listaAutos.modelo + ' . Fabricado en el año ' + listaAutos.anio + ' y del color ' + listaAutos.color
-    }
-}
+    console.log(listaAutos[i]);
+};
